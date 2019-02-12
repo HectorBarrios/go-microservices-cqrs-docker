7ae84dc90c41dc87b481feb1b778966db46111ba
Hector Barrios <hector.br90@gmail.com>
2019-02-12 23:44:13 +0100

    first commit

---
+++ meow-service/handlers.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/HectorBarrios/go-microservices-cqrs-docker/db"
+	"github.com/HectorBarrios/go-microservices-cqrs-docker/event"
+	"github.com/HectorBarrios/go-microservices-cqrs-docker/schema"
+	"github.com/HectorBarrios/go-microservices-cqrs-docker/util"
+	"github.com/segmentio/ksuid"
+)
+
+func createMeowHandler(w http.ResponseWriter, r *http.Request) {
+	type response struct {
+		ID string `json:"id"`
+	}
+
+	ctx := r.Context()
+
+	// Read parameters
+	body := template.HTMLEscapeString(r.FormValue("body"))
+	if len(body) < 1 || len(body) > 140 {
+		util.ResponseError(w, http.StatusBadRequest, "Invalid body")
+		return
+	}
+
+	// Create meow
+	createdAt := time.Now().UTC()
+	id, err := ksuid.NewRandomWithTime(createdAt)
+	if err != nil {
+		util.ResponseError(w, http.StatusInternalServerError, "Failed to create meow")
+		return
+	}
+	meow := schema.Meow{
+		ID:        id.String(),
+		Body:      body,
+		CreatedAt: createdAt,
+	}
+	if err := db.InsertMeow(ctx, meow); err != nil {
+		log.Println(err)
+		util.ResponseError(w, http.StatusInternalServerError, "Failed to create meow")
+		return
+	}
+
+	// Publish event
+	if err := event.PublishMeowCreated(meow); err != nil {
+		log.Println(err)
+	}
+
+	// Return new meow
+	util.ResponseOk(w, response{ID: meow.ID})
+}
